37524195dc6d9ab707cb30b2c116cc92aba32c4d
Try-Fold <tryfoldmusic@gmail.com>
2014-01-22 13:33:00 -0600

    Edited to display GUI, not extend BlockWorkbench

--- treyg/blocksomore/blocks/BlockCrafting.java
+++ treyg/blocksomore/blocks/BlockCrafting.java
@@ -1,26 +1,31 @@
 package treyg.blocksomore.blocks;
 
 import java.util.List;
+import java.util.Random;
 
 import treyg.blocksomore.mod_BlocksOMore;
+import treyg.blocksomore.tileentity.TileEntityCandle;
 import treyg.blocksomore.tileentity.TileEntityCraft;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
+import net.minecraft.block.BlockContainer;
 import net.minecraft.block.BlockWorkbench;
+import net.minecraft.block.material.Material;
 import net.minecraft.client.renderer.texture.IconRegister;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.Icon;
 import net.minecraft.world.World;
 
-public class BlockCrafting extends BlockWorkbench
+public class BlockCrafting extends BlockContainer
 {
 
 	public BlockCrafting(int par1)
 	{
-		super(par1);
-		this.setHardness(1F);
+		super(par1, Material.clay);
+		this.setHardness(0F);
 		this.setResistance(1F);
 	}
 	
@@ -67,6 +72,19 @@ public class BlockCrafting extends BlockWorkbench
 	{
 		par1IconRegister.registerIcon("blocksomore/textures/blocks/craftTable.png");
 	}
+	
+	 public boolean onBlockActivated(World par1World, int par2, int par3, int par4, EntityPlayer par5EntityPlayer, int par6, float par7, float par8, float par9)
+	    {
+	        if (par1World.isRemote)
+	        {
+	            return true;
+	        }
+	        else
+	        {
+	            par5EntityPlayer.displayGUIWorkbench(par2, par3, par4);
+	            return true;
+	        }
+	    }
 
 
 }
