2e2c08c97676625d875452e234c552ceab2ad845
Try-Fold <tryfoldmusic@gmail.com>
2014-01-22 12:52:28 -0600

    um...don't need this

---
+++ treyg/blocksomore/Names.java
@@ -0,0 +1,17 @@
+package treyg.blocksomore;
+
+public class Names 
+{
+	public static final String[] Pillar_Unlocalized = {
+		"Oak Pillar",
+		"Spruce Pillar",
+		"Birch Pillar",
+		"Jungle Pillar"
+		};
+	public static final String[] Pillar_Localized = {
+		"Oak Pillar",
+		"Spruce Pillar",
+		"Birch Pillar",
+		"Jungle Pillar"
+		};
+}
